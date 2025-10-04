1edaa397466a01521055af926d01f50367cba9dc
Tuandev18 <phamtuan180704@gmail.com>
2025-10-04 17:37:43 +0700

    Study gRPC

--- gRPC/server.js
+++ gRPC/server.js
@@ -1,1 +1,26 @@
-console.log("Hello Server");
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+
+const packageDef = protoLoader.loadSync("proto/helloworld.proto");
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const greeter = grpcObject.helloworld.Greeter;
+
+const server = new grpc.Server();
+
+// Cài đặt hàm RPC
+server.addService(greeter.service, {
+  SayHello: (call, callback) => {
+    const name = call.request.name;
+    callback(null, { message: `Xin chào, ${name}!` });
+  },
+});
+
+// Chạy server
+server.bindAsync(
+  "0.0.0.0:50051",
+  grpc.ServerCredentials.createInsecure(),
+  () => {
+    console.log("✅ gRPC server đang chạy tại port 50051");
+    server.start();
+  }
+);
