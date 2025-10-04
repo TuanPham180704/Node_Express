560ea42685e3aad2d1c8ff82842db1f093df4ecc
Tuandev18 <phamtuan180704@gmail.com>
2025-10-04 15:46:19 +0700

    Done mini project Socket_IO

--- Socket_IO/src/server.js
+++ Socket_IO/src/server.js
@@ -6,3 +6,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+io.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected`);
+  socket.on("chat message", (msg) => {
+    console.log("Message :", msg);
+    io.emit("chat message : ", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} disconnect`);
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
