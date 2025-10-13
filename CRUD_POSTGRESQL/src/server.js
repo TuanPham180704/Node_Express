499ecf11c4f8a89e376b5778ed13746a11e1bb8e
Tuandev18 <phamtuan180704@gmail.com>
2025-10-13 21:59:11 +0700

    Done CRUD_POSTGRESQL

--- CRUD_POSTGRESQL/src/server.js
+++ CRUD_POSTGRESQL/src/server.js
@@ -7,25 +7,26 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello PostgreSQL Backend!");
+  res.send("Hello NodeJs ProgreSQL !!!");
 });
+
 app.post("/users", async (req, res) => {
   const { name, email, age } = req.body;
   if (!name || !email || !age)
-    return res.status(404).json({ msg: "Missing Filed!" });
+    return res.status(404).json({ msg: "Missing Filed" });
   try {
-    const user = await pool.query(
-      "INSERT INTO users (name,email,age) VALUES($1,$2,$3) RETURNING *",
+    const result = await pool.query(
+      "INSERT INTO users (name,email,age) VALUES ($1,$2,$3) RETURNING *",
       [name, email, age]
     );
-    res.status(201).json(user.rows[0]);
+    res.json(result.rows[0]);
   } catch (error) {
-    return res.status(404).json({ err: error.message });
+    return res.status(500).json({ error: error.message });
   }
 });
 app.get("/users", async (req, res) => {
   const { name, email, age } = req.query;
-  let query = "SELECT * FROM users";
+  const query = "SELECT * FROM users";
   const params = [];
   if (name) {
     params.push(name);
@@ -38,54 +39,56 @@ app.get("/users", async (req, res) => {
   if (age) {
     params.push(age);
     query +=
-      params.length > 1
+      params.length >= 1
         ? `AND age=$${params.length}`
         : `WHERE age=$${params.length}`;
   }
   try {
     const result = await pool.query(query, params);
     res.json(result.rows);
-  } catch (error) {}
-});
-app.delete("/users/:id", async (req, res) => {
-  const { id } = req.params;
-  let query = "DELETE FROM users WHERE id=$1 RETURNING *";
-  try {
-    const result = await pool.query(query, [id]);
-    if (result.rows.length === 0)
-      return res.status(404).json({ msg: "User not found" });
-    res.json({ delete: result.rows[0] });
   } catch (error) {
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: error.message });
   }
 });
 app.get("/users/:id", async (req, res) => {
   const id = parseInt(req.params.id, 10);
-  if (isNaN(id)) return res.status(404).json({ msg: "Invalid id" });
   const query = "SELECT * FROM users WHERE id=$1";
+  if (isNaN(id)) return res.status(404).json({ msg: "Invalid id" });
   try {
     const result = await pool.query(query, [id]);
+    if (result.rows.length === 0)
+      return res.status(404).json({ msg: "User not found" });
     res.json(result.rows[0]);
   } catch (error) {
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: error.message });
+  }
+});
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const query = "DELETE  FROM users WHERE id=$1 RETURNING * ";
+  try {
+    const result = await pool.query(query, [id]);
+    if (result.rows.length === 0)
+      return res.status(404).json({ msg: "User not found" });
+    res.json({ delete: result.rows });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
 app.put("/users/:id", async (req, res) => {
   const { id } = req.params;
-  const { name, email, age } = req.body;
+  const { name, age, email } = req.body;
+  const query =
+    "UPDATE users SET name=$1,age=$2,email=$3 WHERE id=$4 RETURNING *";
   try {
-    const result = await pool.query(
-      "UPDATE users SET name=$1, email=$2, age=$3 WHERE id=$4 RETURNING *",
-      [name, email, age, id]
-    );
+    const result = await pool.query(query, [name, age, email, id]);
     if (result.rows.length === 0)
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ msg: "User not found" });
     res.json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
-
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
