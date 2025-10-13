a93b3302382e2ea6694b2823091e5217a2754ed9
Tuandev18 <phamtuan180704@gmail.com>
2025-10-13 17:29:48 +0700

    CRUD POSTGRE SQL WITH HTTP

--- CRUD/src/server.js
+++ CRUD/src/server.js
@@ -1,48 +1,51 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const localhost = "localhost";
 const users = require("./data/user");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello NodeJs");
+  res.send("Hello Node Js ");
 });
-
 app.get("/users", (req, res) => {
   res.json(users);
 });
-
 app.get("/users/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const user = users.find((u) => u.id === id);
+  const id = parseFloat(req.params.id);
+  const user = users.find((u) => u.id == id);
   if (!user) return res.status(404).json({ msg: "User not found" });
   res.json(user);
 });
-
 app.post("/users", (req, res) => {
   const newUser = req.body;
   users.push(newUser);
-  res.status(201).json({ msg: "add user success" });
+  res.status(201).json({ msg: "Add User Success" });
 });
-
 app.put("/users/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = parseFloat(req.params.id);
   const index = users.findIndex((u) => u.id === id);
-  if (index !== -1) {
+  if (index !== 1) {
     users[index] = { ...users[index], ...req.body };
     res.json(users[index]);
   } else {
     return res.status(404).json({ msg: "User not found" });
   }
 });
-
 app.delete("/users/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = parseFloat(req.params.id);
   if (!id) return res.status(404).json({ msg: "User not found" });
   users.splice(id, 1);
-  res.json({ msg: "Delete succsess" });
+  res.json({ msg: "Delete success" });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on Port http://localhost:${port}`);
+  console.log(`Sever is running on Port http://${localhost}:${port}`);
 });
+
+
+
+
+
+Pc05gg0744890
--- CRUD_POSTGRESQL/src/server.js
+++ CRUD_POSTGRESQL/src/server.js
@@ -47,6 +47,45 @@ app.get("/users", async (req, res) => {
     res.json(result.rows);
   } catch (error) {}
 });
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  let query = "DELETE FROM users WHERE id=$1 RETURNING *";
+  try {
+    const result = await pool.query(query, [id]);
+    if (result.rows.length === 0)
+      return res.status(404).json({ msg: "User not found" });
+    res.json({ delete: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.get("/users/:id", async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (isNaN(id)) return res.status(404).json({ msg: "Invalid id" });
+  const query = "SELECT * FROM users WHERE id=$1";
+  try {
+    const result = await pool.query(query, [id]);
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email, age } = req.body;
+  try {
+    const result = await pool.query(
+      "UPDATE users SET name=$1, email=$2, age=$3 WHERE id=$4 RETURNING *",
+      [name, email, age, id]
+    );
+    if (result.rows.length === 0)
+      return res.status(404).json({ error: "User not found" });
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
