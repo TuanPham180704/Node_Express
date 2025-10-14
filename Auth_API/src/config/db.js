6b5952f9286301525fc89a5d9b7f863a018c80b5
Tuandev18 <phamtuan180704@gmail.com>
2025-10-14 16:11:52 +0700

    Sequelize & PostgreSQL && Model User

---
+++ Auth_API/src/config/db.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+  }
+);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connected successfully");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error);
+  }
+})();
+
+module.exports = sequelize;
